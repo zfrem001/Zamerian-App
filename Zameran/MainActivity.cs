9f9d222087f2ab62d1fde97e8853ee1576a76657
Zico Fremont <zico.fremont@gmail.com>
2017-10-24 15:51:16 -0400

    Add project files.

---
+++ Zameran/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Zameran
+{
+    [Activity(Label = "Zameran", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView txtNumber;
+
+        int number;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+        }
+    }
+}
+
